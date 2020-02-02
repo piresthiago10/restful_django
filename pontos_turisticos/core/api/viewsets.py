93f2108f4c6bf8504c0485250709116530379df6
Thiago <piresthiago@oi.com.br>
2020-02-02 17:43:50 -0300

    recursos avancados da api

--- pontos_turisticos/core/api/viewsets.py
+++ pontos_turisticos/core/api/viewsets.py
@@ -1,10 +1,45 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
+from rest_framework.decorators import action
 
 
 class PontoTuristicoViewSet(ModelViewSet):
-    queryset = PontoTuristico.objects.all()
     # Serializer são os campos para serem exibidos
     # no json
     serializer_class = PontoTuristicoSerializer
+
+    def get_queryset(self):
+        return PontoTuristico.objects.filter(aprovado=True)
+
+    def list(self, request, *args, **kwargs):
+        return Response({'teste': 123})
+    
+    def create(self, request, *args, **kwargs):
+        return Response({'Hello': request.data['nome']})
+    
+    def destroy(self, request, *args, **kwargs):
+        pass
+
+    def retrieve(self, request, *args, **kwargs):
+        pass
+
+    def update(self, request, *args, **kwargs):
+        pass
+
+    def partial_update(self, request, *args, **kwargs):
+        pass
+
+    ''' Actrions, é um meio de implementar funções sem inflingir
+    os conceitos do rest.
+    http://[ip:porta]/pontosturisticos/pk/denunciar/
+    '''
+    @action(methods=['post', 'get'], detail=True)
+    def denunciar(self, request, pk=None):
+        pass
+
+    # action no endpoint como um todo
+    @action(methods=['get'], detail=False)
+    def teste(self, request):
+        pass
