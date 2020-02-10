9ee84de5ba2a8cb3083b9ad068d44f77707659dd
Thiago <piresthiago@oi.com.br>
2020-02-09 22:17:55 -0300

    implementacao de imagens

--- pontos_turisticos/core/api/viewsets.py
+++ pontos_turisticos/core/api/viewsets.py
@@ -14,22 +14,23 @@ class PontoTuristicoViewSet(ModelViewSet):
         return PontoTuristico.objects.filter(aprovado=True)
 
     def list(self, request, *args, **kwargs):
-        return Response({'teste': 123})
+        return super(PontoTuristicoViewSet, self).list(request, *args, **kwargs)
     
     def create(self, request, *args, **kwargs):
-        return Response({'Hello': request.data['nome']})
+        # o super obtem tudo que a classe mestra faz
+        return super(PontoTuristicoViewSet, self).create(request, *args, **kwargs)
     
     def destroy(self, request, *args, **kwargs):
-        pass
+        return super(PontoTuristicoViewSet, self).destroy(request, *args, **kwargs)
 
     def retrieve(self, request, *args, **kwargs):
-        pass
+        return super(PontoTuristicoViewSet, self).retrieve(request, *args, **kwargs)
 
     def update(self, request, *args, **kwargs):
-        pass
+        return super(PontoTuristicoViewSet, self).update(request, *args, **kwargs)
 
     def partial_update(self, request, *args, **kwargs):
-        pass
+        return super(PontoTuristicoViewSet, self).partial_update(request, *args, **kwargs)
 
     ''' Actrions, é um meio de implementar funções sem inflingir
     os conceitos do rest.
